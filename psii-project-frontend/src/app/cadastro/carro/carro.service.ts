a0c5f33a5139467e23f63496bafc946b1c44972d
LucasLazzarini <lucassintralazzarini@gmail.com>
2019-05-01 21:20:24 -0300

    Adicionando service de carros

---
+++ psii-project-frontend/src/app/cadastro/carro/carro.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../api.service';
+import { id } from '@swimlane/ngx-datatable/release/utils';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarroService extends ApiService {
+
+  private carroUrl = 'carros';
+
+  getCarros() {
+    return this.readAll<any>(this.carroUrl, null);
+  }
+
+  getCarro(id) {
+    return this.read<any>(this.carroUrl, id);
+  }
+
+  saveCarro(carro) {
+    return this.create<any>(this.carroUrl, carro);
+  }
+
+  updateCarro(carro) {
+    return this.update(this.carroUrl, carro.id, carro);
+  }
+}
+
