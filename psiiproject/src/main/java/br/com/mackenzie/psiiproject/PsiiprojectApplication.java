f3dd55fcfa83eb373bebff194f7883bd5a9d1cd2
joaocampari <joaocampari@gmail.com>
2019-05-10 18:16:05 -0300

    Commit inicial do projeto do BackEnd

---
+++ psiiproject/src/main/java/br/com/mackenzie/psiiproject/PsiiprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.mackenzie.psiiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiiprojectApplication.class, args);
+	}
+
+}
